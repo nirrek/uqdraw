d3a89a5b195aebcc3b8e6b60b68c950ab6d3ffe3
Dominic English <dominic.b.english@gmail.com>
2015-04-10 18:47:28 +1000

    move timer out of Question and into Presenter

--- js/components/Presenter.js
+++ js/components/Presenter.js
@@ -1,27 +1,51 @@
 import React from 'react';
 import { Button } from './UI';
 let Firebase = require('firebase');
+let StyleSheet = require('react-style');
+let objectAssign = require('object-assign');
 
 class Presenter extends React.Component {
 
   constructor(props) {
     this.state = {
       activeLectureKey: props.routeParams.lectureId,
-      activeQuestionKey: null,
+      activeQuestionKey: undefined,
       questions: ['Draw the TCP handshake', 'Draw a HTTP connection', 'Draw a picture of a large bird with dragon wings'],
       responses: []
     };
     for (let i = 0; i < 20; i++) {
       this.state.responses.push({thumbnail: 'http://placehold.it/50x50'});
     }
+
+    this.start = this.start.bind(this);
+    this.stop = this.stop.bind(this);
+    this.reset = this.reset.bind(this);
   }
 
   onActivateQuestion(key) {
     this.setState({activeQuestionKey: key});
+    this.reset();
   }
 
   onThumbnailClick(key) {
     console.log('make a large version of submition '+key);
+  }
+
+  start() {
+    this.setState({takingQuestions: true});
+    this.refs.timer.startTimer();
+  }
+
+  stop() {
+    this.setState({takingQuestions: false});
+    this.refs.timer.stopTimer();
+  }
+
+  reset() {
+    this.setState({takingQuestions: false});
+    if (this.refs.timer) {
+      this.refs.timer.resetTimer();
+    }
   }
 
   render() {
@@ -31,22 +55,26 @@ class Presenter extends React.Component {
         display: 'flex',
         flexDirection: 'row',
         position: 'absolute',
-        top: 0,
+        top: 83,
         right: 0,
         bottom: 0,
         left: 0,
-        background: '#eeedff',
+        background: '#FBFAFC',
+        color: '#543C9C',
       },
-      questionPanel: {
+      presentationPanel: {
         display: 'flex',
         flexDirection: 'column',
         flexGrow: 1,
       },
       heading: {
         display: 'flex',
-        background: '#5C42AB',
-        padding: 20,
-        color: '#DDE0FF',
+        margin: 20,
+      },
+      dropShadow: {
+        border: '1px solid #eee',
+        borderBottomColor: '#ddd',
+        boxShadow: '0 1px 1px rgba(0,0,0,.15)',
       },
       presenterCode: {
         display: 'flex',
@@ -67,25 +95,47 @@ class Presenter extends React.Component {
         alignItems: 'flex-end',
         fontSize: 30,
       },
-      question: {
+      questionPanel: {
         display: 'flex',
-        justifyContent: 'center',
+        justifyContent: 'space-between',
         alignItems: 'center',
+        flexDirection: 'column',
         fontSize: 40,
         padding: 20,
         textAlign: 'center',
         lineHeight: '1.3em',
         flexGrow: 1,
       },
+      timer: {
+        alignSelf: 'flex-end',
+        color: '#5C42AB',
+      },
       responses: {
         flexGrow: 1,
         borderTop: 'solid 1px #ccc',
-      }
+      },
+      buttons: {
+        fontSize: 18,
+      },
     };
+
+    let timer;
+    if (typeof this.state.activeQuestionKey !== 'undefined') {
+      timer = <Timer interval="1000" increment="1000" ref="timer"/>;
+    } else {
+      timer = null;
+    }
+
+    let button;
+    if (this.state.takingQuestions) {
+      button = <Button key="1" onClick={this.stop}>Stop Taking Responses</Button>;
+    } else {
+      button = <Button key="2" onClick={this.start}>Start Taking Responses</Button>;
+    }
 
     return (
       <div style={this.styles.container}>
-        <div style={this.styles.questionPanel}>
+        <div style={this.styles.presentationPanel}>
           <div style={this.styles.heading}>
             <div style={this.styles.presenterCode}>
               <span style={this.styles.presenterCodeTitle}>CODE</span>
@@ -95,8 +145,14 @@ class Presenter extends React.Component {
               <span>www.uqdraw.com/b3a</span>
             </div>
           </div>
-          <div style={this.styles.question}>
+          <div style={this.styles.questionPanel}>
+            <div style={this.styles.timer}>
+              {timer}
+            </div>
             <Question question={this.state.questions[this.state.activeQuestionKey]}/>
+            <div style={this.styles.buttons}>
+              {button}
+            </div>
           </div>
           <div style={this.styles.responses}>
             <PresenterResponses responses={this.state.responses} onThumbnailClick={this.onThumbnailClick}/>
@@ -158,9 +214,6 @@ class Question extends React.Component {
         fontSize: 24,
         color: '#aaa',
       },
-      buttons: {
-        fontSize: 18,
-      },
     };
 
     this.state = {
@@ -168,18 +221,8 @@ class Question extends React.Component {
     };
   }
 
-  start() {
-    this.setState({takingQuestions: true});
-    this.refs.timer.startTimer();
-  }
-
-  stop() {
-    this.setState({takingQuestions: false});
-    this.refs.timer.stopTimer();
-  }
-
   render() {
-    console.log('rendering');
+
     if (!this.props.question) {
       return (
         <span style={this.styles.unselected}>
@@ -187,21 +230,10 @@ class Question extends React.Component {
         </span>
       );
     }
-    var button;
-    if (this.state.takingQuestions) {
-      button = <Button key="1" onClick={this.stop.bind(this)}>Stop Taking Responses</Button>;
-    } else {
-      button = <Button key="2" onClick={this.start.bind(this)}>Start Taking Responses</Button>;
-    }
-    console.log(button);
+
     return (
       <div>
         <span>{this.props.question}</span>
-
-        <Timer interval="100" increment="1000" ref="timer"/>
-        <div style={this.styles.buttons}>
-          {button}
-        </div>
       </div>
     );
   }
@@ -258,6 +290,7 @@ class Timer extends React.Component {
     };
     this.startTimer = this.startTimer.bind(this);
     this.stopTimer = this.stopTimer.bind(this);
+    this.resetTimer = this.resetTimer.bind(this);
     this.tick = this.tick.bind(this);
   }
 
@@ -270,6 +303,12 @@ class Timer extends React.Component {
   stopTimer() {
     clearInterval(this.timer);
     this.timer = undefined;
+  }
+
+  resetTimer() {
+    console.log('reset');
+    this.stopTimer();
+    this.setState({elapsed: 0});
   }
 
   tick() {
