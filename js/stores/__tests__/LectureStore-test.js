ca679ec539ce1452989d9f5e49cd2d96e34f8ca1
Dominic English <dominic.b.english@gmail.com>
2015-06-02 13:20:25 +1000

    (LectureStore-test): increase test coverage and refactor

--- js/stores/__tests__/LectureStore-test.js
+++ js/stores/__tests__/LectureStore-test.js
@@ -9,12 +9,42 @@ describe('LectureStore', () => {
 
     let testCourseKey = 'testCourseKey';
     let testLectureKey = 'testLectureKey';
-    let testLecture = {
-        title: 'Test Lecture',
-        questions: [
-            'test',
-            'array',
-        ],
+    let testLectures = {
+        one: {
+            title: 'Test Lecture 1',
+            questions: ['test'],
+        },
+        two: {
+            title: 'Test Lecture 2',
+            questions: ['test'],
+        },
+        three: {
+            title: 'Test Lecture 3',
+            questions: ['test'],
+        },
+        four: {
+            title: 'Test Lecture 4',
+            questions: ['test'],
+        },
+    };
+
+    let actionCreateLecture = {
+        type: ActionTypes.LECTURE_CREATE,
+        courseKey: testCourseKey,
+        lectureKey: '',
+        lecture: '',
+    };
+
+    let actionUpdateLectures = {
+        type: ActionTypes.LECTURES_UPDATE,
+        courseKey: testCourseKey,
+        lectures: {},
+    };
+
+    let actionDeleteLecture = {
+        type: ActionTypes.LECTURE_DELETE,
+        courseKey: testCourseKey,
+        lectureKey: '',
     };
 
     beforeEach(() => {
@@ -23,193 +53,237 @@ describe('LectureStore', () => {
         callback = Dispatcher.register.mock.calls[0][0];
     });
 
+
+    /*
+     * Start Tests
+     */
+
     it('registers a callback with the dispatcher', () => {
         expect(Dispatcher.register.mock.calls.length).toBe(1);
     });
 
-    it('initialises with no lectures', () => {
-        let lectures = LectureStore.getAll(testCourseKey);
-        expect(lectures).toEqual({});
-    });
+    describe('Change Listeners', () => {
+        it('should call a single change callback on change', () => {
+            let changeHandler = jest.genMockFunction();
+            LectureStore.addChangeListener(changeHandler);
 
-    describe('LECTURES_CREATE', () => {
-        let actionCreateLecture = {
-            type: ActionTypes.LECTURE_CREATE,
-            courseKey: testCourseKey,
-            lectureKey: testLectureKey,
-            lecture: testLecture,
-        };
+            LectureStore.emitChange();
 
-        it('adds a single lecture', () => {
-            callback(actionCreateLecture);
-            let lectures = LectureStore.getAll(testCourseKey);
-            let keys = Object.keys(lectures);
-            expect(keys.length).toBe(1);
-            expect(lectures[keys[0]].title).toEqual('Test Lecture');
+            jest.runAllTicks();
+            expect(changeHandler).toBeCalled();
+        });
+
+        it('should call multiple change callbacks on change', () => {
+            let changeHandlers = [
+                jest.genMockFunction(),
+                jest.genMockFunction(),
+            ];
+            LectureStore.addChangeListener(changeHandlers[0]);
+            LectureStore.addChangeListener(changeHandlers[1]);
+
+            LectureStore.emitChange();
+
+            jest.runAllTicks();
+            expect(changeHandlers[0]).toBeCalled();
+            expect(changeHandlers[1]).toBeCalled();
         });
     });
 
-    describe('LECTURES_UPDATE', () => {
-        let actionUpdateLectures;
-        let testLectures = {
-            one: {
-                title: 'Test Lecture 1',
-                questions: ['test'],
-            },
-            two: {
-                title: 'Test Lecture 2',
-                questions: ['test'],
-            },
-            three: {
-                title: 'Test Lecture 3',
-                questions: ['test'],
-            },
-            four: {
-                title: 'Test Lecture 4',
-                questions: ['test'],
-            },
-        };
+    describe('Actions', () => {
+        describe('LECTURES_CREATE', () => {
 
-        beforeEach(() => {
-            actionUpdateLectures = {
-                type: ActionTypes.LECTURES_UPDATE,
-                courseKey: testCourseKey,
-                lectures: {},
-            };
-        });
+            it('adds a single lecture', () => {
+                actionCreateLecture.lectureKey = 'one';
+                actionCreateLecture.lecture = testLectures.one;
+                callback(actionCreateLecture);
+                let lectures = LectureStore.getAll(testCourseKey);
+                let keys = Object.keys(lectures);
+                expect(keys.length).toBe(1);
+                expect(lectures[keys[0]].title).toEqual(testLectures.one.title);
+            });
 
-        it('adds a set of lectures if none exist already', () => {
-            let newLectures = {
-                one: testLectures.one,
-                two: testLectures.two,
-            };
-            actionUpdateLectures.lectures = newLectures;
-            callback(actionUpdateLectures);
-            let lectures = LectureStore.getAll(testCourseKey);
-            expect(lectures).toEqual({
-                one: testLectures.one,
-                two: testLectures.two,
+            it('emits a change event after adding', () => {
+                actionCreateLecture.lectureKey = 'one';
+                actionCreateLecture.lecture = testLectures.one;
+                let changeHandler = jest.genMockFunction();
+                LectureStore.addChangeListener(changeHandler);
+                callback(actionCreateLecture);
+                jest.runAllTicks();
+                expect(changeHandler).toBeCalled();
             });
         });
 
-        it('combines new lectures with existing lectures', () => {
-            let newLectures = {
-                one: testLectures.one,
-                two: testLectures.two,
-            };
-            actionUpdateLectures.lectures = newLectures;
-            callback(actionUpdateLectures);
+        describe('LECTURES_UPDATE', () => {
+            it('adds a set of lectures if none exist already', () => {
+                let newLectures = {
+                    one: testLectures.one,
+                    two: testLectures.two,
+                };
+                actionUpdateLectures.lectures = newLectures;
+                callback(actionUpdateLectures);
+                let lectures = LectureStore.getAll(testCourseKey);
+                expect(lectures).toEqual({
+                    one: testLectures.one,
+                    two: testLectures.two,
+                });
+            });
 
-            let newerLectures = {
-                three: testLectures.three,
-                four: testLectures.four,
-            };
-            actionUpdateLectures.lectures = newerLectures;
-            callback(actionUpdateLectures);
+            it('combines new lectures with existing lectures', () => {
+                let newLectures = {
+                    one: testLectures.one,
+                    two: testLectures.two,
+                };
+                actionUpdateLectures.lectures = newLectures;
+                callback(actionUpdateLectures);
 
-            let lectures = LectureStore.getAll(testCourseKey);
-            expect(lectures).toEqual({
-                one: testLectures.one,
-                two: testLectures.two,
-                three: testLectures.three,
-                four: testLectures.four,
+                let newerLectures = {
+                    three: testLectures.three,
+                    four: testLectures.four,
+                };
+                actionUpdateLectures.lectures = newerLectures;
+                callback(actionUpdateLectures);
+
+                let lectures = LectureStore.getAll(testCourseKey);
+                expect(lectures).toEqual({
+                    one: testLectures.one,
+                    two: testLectures.two,
+                    three: testLectures.three,
+                    four: testLectures.four,
+                });
+            });
+
+            it('replaces an existing lecture if a new version is in the set', () => {
+                let newLectures = {
+                    one: testLectures.one,
+                    two: testLectures.two,
+                };
+                actionUpdateLectures.lectures = newLectures;
+                callback(actionUpdateLectures);
+
+                // Add a new version of the 'two' property that points to fourth object
+                let newerLectures = {
+                    three: testLectures.three,
+                    two: testLectures.four,
+                };
+                actionUpdateLectures.lectures = newerLectures;
+                callback(actionUpdateLectures);
+
+                let lectures = LectureStore.getAll(testCourseKey);
+                expect(lectures).toEqual({
+                    one: testLectures.one,
+                    two: testLectures.four,
+                    three: testLectures.three,
+                });
+            });
+
+            it('emits a change event after updating', () => {
+                let changeHandler = jest.genMockFunction();
+                LectureStore.addChangeListener(changeHandler);
+                let newLectures = {
+                    one: testLectures.one,
+                };
+                actionUpdateLectures.lectures = newLectures;
+                callback(actionUpdateLectures);
+                expect(changeHandler).toBeCalled();
             });
         });
 
-        it('replaces an existing lecture if a new version is in the set', () => {
-            let newLectures = {
-                one: testLectures.one,
-                two: testLectures.two,
-            };
-            actionUpdateLectures.lectures = newLectures;
-            callback(actionUpdateLectures);
+        describe('LECTURES_DELETE', () => {
+            it('can delete a lecture', () => {
+                let newLectures = {
+                    one: testLectures.one,
+                    two: testLectures.two,
+                };
+                actionUpdateLectures.lectures = newLectures;
+                callback(actionUpdateLectures);
 
-            // Add a new version of the 'two' property that points to fourth object
-            let newerLectures = {
-                three: testLectures.three,
-                two: testLectures.four,
-            };
-            actionUpdateLectures.lectures = newerLectures;
-            callback(actionUpdateLectures);
+                actionDeleteLecture.lectureKey = 'one';
+                callback(actionDeleteLecture);
 
-            let lectures = LectureStore.getAll(testCourseKey);
-            expect(lectures).toEqual({
-                one: testLectures.one,
-                two: testLectures.four,
-                three: testLectures.three,
+                let lectures = LectureStore.getAll(testCourseKey);
+                expect(lectures).toEqual({
+                    two: testLectures.two,
+                });
+            });
+
+            it('does nothing if the lecture doesn\'t exist', () => {
+                let newLectures = {
+                    one: testLectures.one,
+                    two: testLectures.two,
+                };
+                actionUpdateLectures.lectures = newLectures;
+                callback(actionUpdateLectures);
+
+                actionDeleteLecture.lectureKey = 'three';
+                callback(actionDeleteLecture);
+
+                let lectures = LectureStore.getAll(testCourseKey);
+                expect(lectures).toEqual({
+                    one: testLectures.one,
+                    two: testLectures.two,
+                });
+            });
+
+            it('does nothing if there are no lectures', () => {
+                actionDeleteLecture.lectureKey = 'one';
+                callback(actionDeleteLecture);
+
+                let lectures = LectureStore.getAll(testCourseKey);
+                expect(lectures).toEqual({});
+            });
+
+            it('emits a change event after deleting', () => {
+                let changeHandler = jest.genMockFunction();
+                let newLectures = {
+                    one: testLectures.one,
+                    two: testLectures.two,
+                };
+                actionUpdateLectures.lectures = newLectures;
+                callback(actionUpdateLectures);
+
+                LectureStore.addChangeListener(changeHandler);
+                actionDeleteLecture.lectureKey = 'one';
+                callback(actionDeleteLecture);
+                expect(changeHandler).toBeCalled();
             });
         });
     });
 
-    describe('LECTURES_DELETE', () => {
-        let actionUpdateLectures;
-        let actionDeleteLecture;
-        let testLectures = {
-            one: {
-                title: 'Test Lecture 1',
-                questions: ['test'],
-            },
-            two: {
-                title: 'Test Lecture 2',
-                questions: ['test'],
-            },
-        };
+    describe('API', () => {
+        describe('getAll', () => {
+            it('initialises with no lectures', () => {
+                let lectures = LectureStore.getAll(testCourseKey);
+                expect(lectures).toEqual({});
+            });
 
-        beforeEach(() => {
-            actionUpdateLectures = {
-                type: ActionTypes.LECTURES_UPDATE,
-                courseKey: testCourseKey,
-                lectures: {},
-            };
-            actionDeleteLecture = {
-                type: ActionTypes.LECTURE_DELETE,
-                courseKey: testCourseKey,
-                lectureKey: '',
-            };
-        });
+            it('gets all lectures for the course', () => {
+                actionUpdateLectures.lectures = testLectures;
+                callback(actionUpdateLectures);
+                let lectures = LectureStore.getAll(testCourseKey);
+                expect(lectures).toEqual(testLectures);
+            });
 
-        it('can delete a lecture', () => {
-            let newLectures = {
-                one: testLectures.one,
-                two: testLectures.two,
-            };
-            actionUpdateLectures.lectures = newLectures;
-            callback(actionUpdateLectures);
-
-            actionDeleteLecture.lectureKey = 'one';
-            callback(actionDeleteLecture);
-
-            let lectures = LectureStore.getAll(testCourseKey);
-            expect(lectures).toEqual({
-                two: testLectures.two,
+            it('should throw an error if missing an argument', () => {
+                expect(() => LectureStore.getAll()).toThrow();
             });
         });
 
-        it('does nothing if the lecture doesn\'t exist', () => {
-            let newLectures = {
-                one: testLectures.one,
-                two: testLectures.two,
-            };
-            actionUpdateLectures.lectures = newLectures;
-            callback(actionUpdateLectures);
-
-            actionDeleteLecture.lectureKey = 'three';
-            callback(actionDeleteLecture);
-
-            let lectures = LectureStore.getAll(testCourseKey);
-            expect(lectures).toEqual({
-                one: testLectures.one,
-                two: testLectures.two,
+        describe('get', () => {
+            it('should retrieve the correct lecture', () => {
+                actionUpdateLectures.lectures = testLectures;
+                callback(actionUpdateLectures);
+                let lecture = LectureStore.get(testCourseKey, 'three');
+                expect(lecture).toEqual(testLectures.three);
             });
-        });
 
-        it('does nothing if there are no lectures', () => {
+            it('should return undefined if lecture doesn\'t exist', () => {
+                let lecture = LectureStore.get(testCourseKey, 'three');
+                expect(lecture).toBeUndefined();
+            });
 
-            actionDeleteLecture.lectureKey = 'one';
-            callback(actionDeleteLecture);
-
-            let lectures = LectureStore.getAll(testCourseKey);
-            expect(lectures).toEqual({});
+            it('should throw an error if missing an argument', () => {
+                expect(() => LectureStore.get('three')).toThrow();
+            });
         });
     });
 });
