6106149a823f6202367a29be9e31cfd3c2375489
kerrin <kerrinenglish@gmail.com>
2015-06-13 15:15:46 +1000

    (API-integration) restructure to reuse the Firebase
    Instead of instantiating new servers, just set the Firebase
    to the state that we want before beginning a suite

--- js/__integrationTests__/API-integration-test.js
+++ js/__integrationTests__/API-integration-test.js
@@ -3,54 +3,71 @@ describe('API Integration', function() {
   var FirebaseServer = require('firebase-server');
   var server;
 
-  // Used to launch a Firebase with an empty document
-  var launchEmptyServer = function() {
-    return new FirebaseServer(5000, 'test.firebaseio.com', {});
-  };
-
-  // Used to launch a Firebase with a standard-looking document
-  var launchPrepopulatedServer = function() {
-    return new FirebaseServer(5000, 'test.firebaseio.com', {
-      courseLists: {
-        uqjstee8: {
-          '-JlUd0xRBkwULfuGFGqo': 'COMS3200',
-          '-JldDBERX2jaTHTanV-P': 'INFS3202',
-          '-Jli22md1TMXq7YhqMHd': 'DECO3800',
-        }
-      }
-    });
-  };
-
-  afterEach(function() {
-    if (!server) return;
-    server.close();
-    server = null;
+  beforeAll(function() {
+    server = new FirebaseServer(5000, 'test.firebaseio.com', {});
   });
 
   describe('Empty Firebase', function() {
-    beforeEach(function() {
-      server = launchEmptyServer();
+    var client; // Firebase client used in each spec.
+
+    beforeAll(function(done) {
+      var fbClient = new Firebase('ws://test.firebaseio.com:5000');
+
+      // Empty out the Firebase
+      fbClient.set(null, function(error) {
+        if (error) return console.log(error);
+        done();
+      });
+    });
+
+    afterEach(function() {
+      client.off();
+      client = null;
     });
 
     it('is empty to begin', function(done) {
-      var client = new Firebase('ws://test.firebaseio.com:5000');
+      client = new Firebase('ws://test.firebaseio.com:5000');
       client.on('value', function(snapshot) {
         var data = snapshot.val();
-        expect(data).toEqual({});
+        expect(data).toEqual(null);
         done();
       });
     });
   });
 
   describe('Prepopulated Firebase', function() {
+    var client; // Firebase client used in each spec.
+
+    beforeAll(function(done) {
+      var fbClient = new Firebase('ws://test.firebaseio.com:5000');
+      var data = {
+        courseLists: {
+          uqjstee8: {
+            '-JlUd0xRBkwULfuGFGqo': 'COMS3200',
+            '-JldDBERX2jaTHTanV-P': 'INFS3202',
+            '-Jli22md1TMXq7YhqMHd': 'DECO3800',
+          }
+        }
+      };
+
+      // Populate the Firebase with the data
+      fbClient.set(data, function(error) {
+        if (error) return console.log(error);
+        done();
+      });
+    });
+
+    afterEach(function() {
+      client.off();
+      client = null;
+    });
+
     it('finds all the subjects', function(done) {
-      var client = new Firebase('ws://test.firebaseio.com:5000/courseLists');
+      client = new Firebase('ws://test.firebaseio.com:5000/courseLists');
 
       client.on('value', function(snapshot) {
         var data = snapshot.val();
-        console.log(data);
 
-        // Assert expectations
         expect(data).toEqual({
           uqjstee8: {
             '-JlUd0xRBkwULfuGFGqo': 'COMS3200',
