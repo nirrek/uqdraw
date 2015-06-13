72320f8df6df05c9fd43523fb54f1472f2b2eb87
Dominic English <dominic.b.english@gmail.com>
2015-06-13 19:02:49 +1000

    (PresentationConstants): update constants to conform to new style guide

--- js/constants/PresentationConstants.js
+++ js/constants/PresentationConstants.js
@@ -2,9 +2,11 @@ let keyMirror = require('keymirror');
 
 export default {
     ActionTypes: keyMirror({
-            RESPONSE_CREATE: null,
-            RESPONSE_CREATED: null,
+            RESPONSE_CREATE_INITIATED: null,
+            RESPONSE_CREATE_SUCCESS: null,
             RESPONSE_CREATE_FAIL: null,
-            RESPONSES_UPDATE: null,
+            RESPONSES_UPDATE_INITIATED: null,
+            RESPONSES_UPDATE_SUCCESS: null,
+            RESPONSES_UPDATE_FAIL: null,
     }),
 };
