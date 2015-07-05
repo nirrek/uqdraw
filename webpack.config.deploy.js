0d0b191fed0bad15f88d806dc3f3482cda8eccbb
kerrin <kerrinenglish@gmail.com>
2015-07-06 09:47:47 +1000

    (webpack) add loader for jpg/png/gif/svg to minify+optimize

--- webpack.config.deploy.js
+++ webpack.config.deploy.js
@@ -28,8 +28,11 @@ module.exports = {
         ]
       },
       {
-        test: /\.(png|jpg)$/,
-        loader: 'file-loader'
+        test: /\.(jpe?g|png|gif|svg)$/i,
+        loaders: [
+          'file?hash=sha512&digest=hex&name=[hash].[ext]',
+          'image-webpack?bypassOnDebug&optimizationLevel=7&interlaced=false'
+        ]
       }
     ]
   }
