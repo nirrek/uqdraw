1a40a72f667684bbac7db2c1880c5ed265529c04
nirrek <hi@kerr.in>
2016-02-17 21:27:43 +1000

    (Logo) cleanup styles

--- js/components/Logo.jsx
+++ js/components/Logo.jsx
@@ -2,43 +2,14 @@ import React, { Component } from 'react';
 require('../../css/components/Logo.scss');
 
 export default class Logo extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      isHovered: false
-    };
-
-    this.logoStyle = {
-      default: {
-        fontSize: 25,
-        fontWeight: 800,
-        textTransform: 'uppercase',
-        backgroundColor: 'rgba(0,0,0,.1)',
-        borderBottom: '1px solid rgba(0,0,0,.05)',
-        color: '#E1DDED',
-        transition: 'opacity 0.3s ease-out',
-      },
-      hovered: {
-        opacity: 0.8,
-        cursor: 'pointer',
-      },
-    };
+  shouldComponentUpdate() {
+    return false; // immutable component
   }
 
-  mouseOver() { this.setState({ isHovered: true }); }
-
-  mouseOut() { this.setState({ isHovered: false }); }
-
   render() {
-    let logoStyle = this.state.isHovered ? this.logoStyle.hovered : {};
-
     return (
       <div className='Logo' ref='logo'>
-        <img className='Logo-img' src={require('../../images/logo.svg')}
-             onMouseOver={this.mouseOver.bind(this)}
-             onMouseOut={this.mouseOut.bind(this)}
-             style={logoStyle}/>
+        <img className='Logo-img' src={require('../../images/logo.svg')} />
       </div>
     );
   }
