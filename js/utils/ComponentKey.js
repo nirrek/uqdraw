4ace2d7feb8a2802f8fee62b8c085a6c7500e074
kerrin <kerrinenglish@gmail.com>
2016-02-18 19:18:04 +1000

    es6ify ComponentKey

--- js/utils/ComponentKey.js
+++ js/utils/ComponentKey.js
@@ -1,14 +1,9 @@
 /**
- * Library to generate a unique key for a component. Currently just generates
- * a key from the current timestamp and some random numbers
+ * Generates a unique key for a component. Currently just generates
+ * a key from the current timestamp and some random numbers.
  */
-
-let ComponentKey = {
-    generate: () => {
-        var time = Date.now();
-        var random = Math.random() * Math.random();
-        return Math.floor(time * random);
-    },
+export default function generateComponentKey() {
+  const time = Date.now();
+  const random = Math.random() * Math.random();
+  return Math.floor(time * random);
 }
-
-export default ComponentKey;
