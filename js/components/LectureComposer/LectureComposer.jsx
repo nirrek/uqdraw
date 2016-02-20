39b287da6fc5b3a5cb471b847353aa035954be07
kerrin <kerrinenglish@gmail.com>
2016-02-20 23:18:50 +1000

    move LectureComposer to own dir

---
+++ js/components/LectureComposer/LectureComposer.jsx
@@ -0,0 +1,57 @@
+import React, { Component } from 'react';
+import Button from '../Button/Button.jsx';
+import Modal, { ModalFooter, ModalContent } from '../Modal/Modal.jsx';
+import FlatButton from '../FlatButton/FlatButton.jsx';
+import Input from '../Input/Input.jsx';
+import './LectureComposer.scss';
+
+export default class LectureComposer extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      inputText: '',
+    };
+
+    this.onInputChange = this.onInputChange.bind(this);
+    this.onSave = this.onSave.bind(this);
+  }
+
+  onInputChange(event) {
+    this.setState({ inputText: event.target.value });
+  }
+
+  onSave() {
+    this.props.onSave(this.state.inputText);
+    this.setState({ inputText: '' });
+  }
+
+  render() {
+    const { isOpen, onClose } = this.props;
+
+    return (
+      <Modal isOpen={isOpen} onClose={onClose} hasCloseButton={false}>
+        <ModalContent>
+          <div className='LectureCompose-inputContainer'>
+            <Input placeholder='Lecture Name'
+                   value={this.state.inputText}
+                   onChange={this.onInputChange} />
+          </div>
+        </ModalContent>
+        <ModalFooter>
+          <FlatButton type="secondary" onClick={onClose}>
+            Close
+          </FlatButton>
+          <FlatButton type="primary" onClick={this.onSave}>
+            Add Lecture
+          </FlatButton>
+        </ModalFooter>
+      </Modal>
+    );
+  }
+}
+
+LectureComposer.propTypes = {
+  onSave: React.PropTypes.func,
+  isOpen: React.PropTypes.bool,
+  onClose: React.PropTypes.func,
+};
