58feea814f0dcf5b3ce47511fafef42cda1d8ee2
kerrin <kerrinenglish@gmail.com>
2016-02-22 16:54:26 +1000

    (Input) add support for password type inputs and classname merging

--- js/components/Input/Input.jsx
+++ js/components/Input/Input.jsx
@@ -3,11 +3,22 @@ import './Input.scss';
 
 export default class Input extends Component {
   render() {
+    const { type, className, ...rest } = this.props;
+    const classList = className ? `Input ${className}` : 'Input';
+
     return (
       <input
-        type="text"
-        className="Input"
-        {...this.props} />
+        {...rest}
+        type={type}
+        className={classList} />
     );
   }
 }
+
+Input.propTypes = {
+  type: PropTypes.oneOf(['text', 'password'])
+};
+
+Input.defaultProps = {
+  type: 'text'
+};
