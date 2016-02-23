bc37e710c4c4015eeddeeb36311ec7ce3ab04446
kerrin <kerrinenglish@gmail.com>
2016-02-23 19:08:20 +1000

    (Answer) update modal styles

--- js/components/Answer/Answer.jsx
+++ js/components/Answer/Answer.jsx
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import generateComponentKey from '../../utils/ComponentKey.js';
 import PresentationStore from '../../stores/PresentationStore.js';
-import PresentationActions from '../../actions/PresentationActions.js';
-import Drawing from '../../components/Drawing.jsx';
+import { createResponse } from '../../actions/PresentationActions.js';
+import Drawing from '../../components/DrawingTool/DrawingTool.jsx';
 import { subscribe, unsubscribe, APIConstants } from '../../utils/API.js';
 import Button from '../Button/Button.jsx';
-import Modal from '../Modal/Modal.jsx';
+import Modal, { ModalContent, ModalFooter } from '../Modal/Modal.jsx';
+import FlatButton from '../FlatButton/FlatButton.jsx';
+import './Answer.scss';
 
 export default class Answer extends Component {
   constructor(props) {
@@ -53,7 +55,7 @@ export default class Answer extends Component {
     // TODO make programmatic
     const lectureKey = '-JliFPJmDtXhEAv-YRZ4';
     const questionKey = '-JmhCbo1eHVVsTEA4TuZ';
-    PresentationActions.createResponse(lectureKey, questionKey, response);
+    createResponse(lectureKey, questionKey, response);
   }
 
   getPresentationState() {
@@ -66,14 +68,22 @@ export default class Answer extends Component {
 
   render() {
     return (
-      <div className='Drawing'>
+      <div className='Answer'>
         {this.state.isQuestionActive ? (
           <div>
             <Modal isOpen={this.state.isQuestionOpen} onClose={this.hideQuestion} hasCloseButton={false}>
-              <h3>Draw the 3-way handshake TCP uses to establish a connection</h3>
-              <Button onClick={this.hideQuestion}>
-                Tap To Start Drawing
-              </Button>
+              <ModalContent className=''>
+                <h1 className='Answer-question'>
+                  Draw the 3-way handshake TCP uses to establish a connection
+                </h1>
+              </ModalContent>
+              <ModalFooter>
+                <div className='Answer-button'>
+                  <FlatButton onClick={this.hideQuestion}>
+                    Tap To Start Drawing
+                  </FlatButton>
+                </div>
+              </ModalFooter>
             </Modal>
             <Drawing activeQuestionKey="1" onSubmitImage={this.onSubmitImage.bind(this)} isSubmitting={this.state.isSubmitting}/>
           </div>
